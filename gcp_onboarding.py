094751380e5344d3559c43df4317a437825dc649
root <root@Debian-CLI.casa.local>
2022-01-16 17:00:15 -0600

    added delete function in experimental

--- gcp_onboarding.py
+++ gcp_onboarding.py
@@ -73,13 +73,28 @@ def compare_projects_to_add():
 	main_list = np.setdiff1d(projects,projects_d9)
 	return main_list
 
+def compare_projects_to_del():
+        projects = []
+        projects_d9 = []
+        r = requests.get(d9_api + '/v2/GoogleCloudAccount', headers=headers, auth=(d9_api_key, d9_api_secret))
+        data = r.json()
+        for i in gcp_client.list_projects():
+                projects.append(i.project_id)
+                project_d[i.project_id]=i.name
+        for n in data:
+                projects_d9.append(n['projectId'])
+        main_list = np.setdiff1d(projects_d9,projects)
+        return main_list
+
+for i in gcp_client.list_projects():
+	print(i.project_id, i.status)
+
 def add_projects():
 	try:
 		added_accounts = 0
 		print(Fore.WHITE + "===================================================================================================================")
 		print(Fore.YELLOW + "                                             Dome9 GCP Onboarding Tool")
 		print(Fore.WHITE + "===================================================================================================================","\n")
-		print(project_d)
 		for project in compare_projects_to_add():
 			creds_json_file.update({'project_id': project})
 			newval = creds_json_file.get("project_id")
@@ -114,5 +129,43 @@ def add_projects():
 	print(Fore.WHITE + "===================================================================================================================","\n")
 	print(Fore.CYAN + "Number of GCP projects successfully added to Dome9: ", added_accounts)
 	print(Fore.WHITE + "===================================================================================================================","\n")
+
+def del_projects():
+	try:
+                deleted_accounts = 0
+                print(Fore.WHITE + "===================================================================================================================")
+                print(Fore.YELLOW + "                                             Dome9 GCP Offboarding Tool")
+                print(Fore.WHITE + "===================================================================================================================","\n")
+                for project in compare_projects_to_del():
+                        print(Fore.WHITE + "===================================================================================================================")
+                        print(Fore.WHITE + "Project to delete: ",project, "with name:", project_d[project])
+                        r = requests.delete(d9_api + '/v2/GoogleCloudAccount/' + project, headers=headers, auth=(d9_api_key, d9_api_secret))
+                        if r.status_code == 204:
+                                print(Fore.GREEN + 'Project successfully deleted in Dome9:', project_d[project])
+                                print(Fore.WHITE + "===================================================================================================================","\n")
+                                deleted_accounts = deleted_accounts + 1
+                        elif r.status_code == 400:
+                                print(Fore.RED + 'There was an error with the project, please check credentials and that it does not already exist in Dome9')
+                                print("Error message:")
+                                print(r.text,"\n")
+                        elif r.status_code == 401:
+                                print(Fore.RED + 'Bad credentials onboarding project to Dome9:',project_d[project],"\n")
+                        elif r.status_code == 409:
+                                print(Fore.GREEN + 'Project already exists in Dome9:', project_d[project])
+                                print(Fore.WHITE + "===================================================================================================================","\n")
+                        elif r.status_code == 500:
+                                print(Fore.RED + 'Error onboarding project to Dome9, check dependent APIs are enabled in GCP:',project_d[project],"\n")
+                        else:
+                                print(Fore.RED + 'Unknown error onboarding subscription to Dome9:',project_d[project],'Status Code:', r.status_code)
+                                print(r.content,"\n")
+	except:
+		print(Fore.RED + "Unknown error, soz!")
+	print("\n")
+	print(Fore.WHITE + "===================================================================================================================")
+	print(Fore.WHITE + "                                           RUN COMPLETE - SUMMARY RESULTS")
+	print(Fore.WHITE + "===================================================================================================================","\n")
+	print(Fore.CYAN + "Number of GCP projects successfully deleted from Dome9: ", deleted_accounts)
+	print(Fore.WHITE + "===================================================================================================================","\n")
+
 
 add_projects()
